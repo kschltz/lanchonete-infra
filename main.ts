d9bd097de075e1157d8b02690eb76f7cb20d51d1
Kaue Schultz <kaue.schultz@beacon.works>
2024-03-15 22:18:32 -0300

    qubreado

--- main.ts
+++ main.ts
@@ -1,33 +1,24 @@
 import {Construct} from "constructs";
-import {App, TerraformStack, TerraformVariable} from "cdktf";
+import {App, DataTerraformRemoteStateLocal, TerraformStack, TerraformVariable} from "cdktf";
 import {AwsProvider, AwsProviderConfig, AwsProviderDefaultTags} from "@cdktf/provider-aws/lib/provider";
-import {SecurityGroup} from "@cdktf/provider-aws/lib/security-group";
-import {Vpc} from "@cdktf/provider-aws/lib/vpc";
-import {EksCluster} from "@cdktf/provider-aws/lib/eks-cluster";
-import {IamRole} from "@cdktf/provider-aws/lib/iam-role";
-import {Subnet} from "@cdktf/provider-aws/lib/subnet";
-import {IamRolePolicyAttachment} from "@cdktf/provider-aws/lib/iam-role-policy-attachment";
 import {EcrRepository} from "@cdktf/provider-aws/lib/ecr-repository";
-import {InternetGateway} from "@cdktf/provider-aws/lib/internet-gateway";
-import {NatGateway} from "@cdktf/provider-aws/lib/nat-gateway";
-import {RouteTable} from "@cdktf/provider-aws/lib/route-table";
-import {Route} from "@cdktf/provider-aws/lib/route";
-import {Eip} from "@cdktf/provider-aws/lib/eip";
-import {EksNodeGroup} from "@cdktf/provider-aws/lib/eks-node-group";
+import * as Eks from "./.gen/modules/terraform-aws-modules/aws/eks";
+import {VPCStack} from "./modules/vpcStack";
 
 class LanchoneteStack extends TerraformStack {
-    constructor(scope: Construct, id: string) {
+    constructor(scope: Construct, id: string, vpcStack: VPCStack) {
         super(scope, id);
         const AWS_ACCESS_KEY_ID = new TerraformVariable(this, "AWS_ACCESS_KEY_ID", {type: "string", sensitive: true});
         const AWS_SECRET_ACCESS_KEY = new TerraformVariable(this, "AWS_SECRET_ACCESS_KEY", {
             type: "string",
             sensitive: true
         });
-
+        console.log(vpcStack.vpcId);
         const tags: AwsProviderDefaultTags[] = [
             {
                 tags: {
                     'environment': 'dev',
+                    'project': 'lanchonete'
                 },
             },
         ];
@@ -39,136 +30,81 @@ class LanchoneteStack extends TerraformStack {
         }
         new AwsProvider(this, 'aws-provider', cfg);
 
-        const vpc = new Vpc(this, 'eksVpc', {
-            cidrBlock: '10.0.0.0/16',
 
-        });
-
-        console.log(cfg);
-
-        const subnet = new Subnet(this, 'eksSubnet', {
-            vpcId: vpc.id,
-            cidrBlock: '10.0.1.0/24',
-            availabilityZone: 'us-east-1a',
-            mapPublicIpOnLaunch: true
-        });
-
-        const subnet2 = new Subnet(this, 'eksSubnet2', {
-            vpcId: vpc.id,
-            cidrBlock: '10.0.2.0/24',
-            availabilityZone: 'us-east-1b',
-            mapPublicIpOnLaunch: true
-        });
-
-        const securityGroup = new SecurityGroup(this, 'eksSecurityGroup', {
-            vpcId: vpc.id,
-            ingress: [{
-                fromPort: 0,
-                toPort: 0,
-                protocol: '-1',
-                cidrBlocks: ['0.0.0.0/0']
-            }],
-            egress: [{
-                fromPort: 0,
-                toPort: 0,
-                protocol: '-1',
-                cidrBlocks: ['0.0.0.0/0']
-            }]
-        });
-
-        const eksRole = new IamRole(this, 'eksRole', {
-            assumeRolePolicy: JSON.stringify({
-                Version: '2012-10-17',
-                Statement: [{
-                    Effect: 'Allow',
-                    Principal: {
-                        Service: 'eks.amazonaws.com'
-                    },
-                    Action: 'sts:AssumeRole'
-                }]
-            })
-        });
-
-        const nodeGroupRole = new IamRole(this, 'eksNodeGroupRole', {
-            assumeRolePolicy: JSON.stringify({
-                Version: '2012-10-17',
-                Statement: [{
-                    Effect: 'Allow',
-                    Principal: {
-                        Service: 'ec2.amazonaws.com'
-                    },
-                    Action: 'sts:AssumeRole'
-                }]
-            })
-        });
-        new IamRolePolicyAttachment(this, 'eksNodeGroupRolePolicyAttachment1', {
-            role: nodeGroupRole.name,
-            policyArn: 'arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy'
-        });
-
-
-        new IamRolePolicyAttachment(this, 'eksRolePolicyAttachment2', {
-            role: eksRole.name,
-            policyArn: 'arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy'
-        });
-
-        new IamRolePolicyAttachment(this, 'eksRolePolicyAttachment3', {
-            role: eksRole.name,
-            policyArn: 'arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly'
-        });
-
-
-        new EcrRepository(this, 'eksEcrRepository', {
+        new EcrRepository(this, 'ecr-lanchonete-api', {
             name: "lanchonete-api",
-            forceDelete: true
+            forceDelete: true,
+
         });
 
-        const eks = new EksCluster(this, 'eksCluster', {
-            name: 'my-eks-cluster',
-            roleArn: eksRole.arn,
-            vpcConfig: {
-                subnetIds: [subnet.id, subnet2.id],
-                securityGroupIds: [securityGroup.id],
-                endpointPublicAccess: true,
-                endpointPrivateAccess: true
-            }
-        });
-        new EksNodeGroup(this, 'eksNodeGroup', {
-            clusterName: eks.name,
-            nodeGroupName: 'eks-node-group',
-            nodeRoleArn: nodeGroupRole.arn,
-            subnetIds: [subnet.id, subnet2.id],
-            scalingConfig: {
-                desiredSize: 2,
-                maxSize: 4,
-                minSize: 1
-            },
-            instanceTypes: ['t3.medium'],
-            diskSize: 20
-        });
-        new InternetGateway(this, 'InternetGateway', {
-            vpcId: vpc.id,
+        const localState = new DataTerraformRemoteStateLocal(this, "vpc-state", {
+            path: "terraform.vpc-infra.tfstate",
         });
 
-        const eip = new Eip(this, 'Eip', {});
+        console.log(
+            localState.toString(),
+            "vpc_id", localState.getString("vpc_id"),
+            "vpc_private_subnets", localState.getString("vpc_private_subnets"),
+            "vpc_public_subnets", localState.getString("vpc_public_subnets"),
+            "vpc_intra_subnets", localState.getString("vpc_intra_subnets"));
 
-        const natGateway = new NatGateway(this, 'NatGateway', {
-            allocationId: eip.id,
-            subnetId: subnet.id,
-        });
-
-        const routeTable = new RouteTable(this, 'RouteTable', {
-            vpcId: vpc.id,
-        });
-
-        new Route(this, 'Route', {
-            routeTableId: routeTable.id,
-            destinationCidrBlock: '0.0.0.0/0',
-            natGatewayId: natGateway.id,
+        new Eks.Eks(this, "eks", {
+            clusterAddons: [
+                {
+                    coredns: [
+                        {
+                            most_recent: true,
+                        },
+                    ],
+                    "kube-proxy": [
+                        {
+                            most_recent: true,
+                        },
+                    ],
+                    "vpc-cni": [
+                        {
+                            most_recent: true,
+                        },
+                    ],
+                },
+            ],
+            clusterEndpointPublicAccess: true,
+            clusterName: "mba-fiap-cluster",
+            controlPlaneSubnetIds: localState.getList("vpc_public_subnets"),
+            eksManagedNodeGroupDefaults: [
+                {
+                    ami_type: "AL2_x86_64",
+                    attach_cluster_primary_security_group: true,
+                    instance_types: ["m5.large"],
+                },
+            ],
+            eksManagedNodeGroups: [
+                {
+                    "ascode-cluster-wg": [
+                        {
+                            capacity_type: "SPOT",
+                            desired_size: 1,
+                            instance_types: ["t3.large"],
+                            max_size: 2,
+                            min_size: 1,
+                            tags: {
+                                ExtraTag: "helloworld",
+                            },
+                        },
+                    ],
+                },
+            ],
+            subnetIds: localState.getList("vpc_private_subnets"),
+            tags: tags[0].tags,
+            vpcId: localState.getString("vpc_id"),
         });
     }
+
+
 }
 
 const app = new App();
-new LanchoneteStack(app, "lanchonete-infra");
+const vpcStack = new VPCStack(app, "vpc-infra");
+const lanchoneteStack = new LanchoneteStack(app, "lanchonete-infra", vpcStack);
+lanchoneteStack.addDependency(vpcStack);
 app.synth();
